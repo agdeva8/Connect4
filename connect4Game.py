0e33d878ae607a14f0c3735b7f8f950e358f2be8
agdeva8 <agdeva8>
2020-04-05 14:35:02 +0000

    added players and valid configuration

--- connect4Game.py
+++ connect4Game.py
@@ -7,6 +7,7 @@ BLACK = (0,0,0)
 RED = (255,0,0)
 GREEN = (0,255,0)
 BLUE = (0,0,255)
+YELLOW = (255, 255, 0)
 
 displayWidth = 500;     displayHeight = 700;
 startX = 50; startY = 50;
@@ -42,7 +43,26 @@ def fillGrid(DISPLAY):
 def createDisk(DISPLAY, row, col, color):
     x = int((xvals[col] + xvals[col + 1]) / 2) + 1;
     y = int((yvals[row] + yvals[row + 1]) / 2) + 1;
-    pygame.draw.circle(DISPLAY, color, (x, y), int(cellWidth / 2) - 1, 0); 
+    pygame.draw.circle(DISPLAY, color, (x, y), int(cellWidth / 2) - 5, 0); 
+
+def changeTurn(playerID):
+    if playerID == 0:
+        return 1
+    return 0
+
+def checkValidity(row, col):
+    return (row >= 0 and row < numRows and col >= 0 and numCols < numCols);
+
+def updateConfig(DISPLAY, currConfig, row, col, color):
+    if checkValidity(row, col):
+        return False
+
+    if currConfig[col] == -1:
+        return False
+
+    createDisk(DISPLAY, currConfig[col], col, color)
+    currConfig[col] = currConfig[col] - 1
+    return True
 
 def main():
     pygame.init()
@@ -58,7 +78,15 @@ def main():
         for col in range(0, numCols):
             createDisk(DISPLAY, row, col, WHITE)
 
+    currConfig = [numRows - 1]*numCols
+    diskColor = [YELLOW, RED]
+    # if player == 1:
+    #     diskColor = YELLOW
+    # else:
+    #     diskColor = RED
+
     # main loop to capture events
+    playerID = 0;
     while True:
         for event in pygame.event.get():
             if event.type==QUIT:
@@ -68,10 +96,10 @@ def main():
                 if event.button == 1:
                     x = event.pos[0];                       y = event.pos[1]
                     colNum = (x - startX) / cellWidth;      rowNum = (y - startY) / cellHeight 
-                    if (rowNum < numRows and colNum < numCols): 
-                        print("Col Num is {} and row Num is {}".format(rowNum, colNum))
-                        createDisk(DISPLAY, rowNum, colNum, GREEN)
-
+                    # if (rowNum < numRows and colNum < numCols): 
+                    print("Col Num is {} and row Num is {}".format(rowNum, colNum))
+                    # createDisk(DISPLAY, rowNum, colNum, diskColor[playerID])
+                    if updateConfig(DISPLAY, currConfig, rowNum, colNum, diskColor[playerID]):
+                        playerID = changeTurn(playerID)
         pygame.display.update()
-
 main()
