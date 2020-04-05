7de74723d31a34ecad877c8cdfac10d9e919e003
agdeva8 <agdeva8>
2020-04-05 13:30:32 +0000

    change grid dimension from 7x7 to 6x7

--- connect4Game.py
+++ connect4Game.py
@@ -11,14 +11,15 @@ BLUE = (0,0,255)
 displayWidth = 500;     displayHeight = 700;
 startX = 50; startY = 50;
 lineWidth = 2;
-numLines = 7;
+numRows = 6
+numCols = 7
 lineColor = BLUE; 
 
 cellWidth = 50; 
 cellHeight = 50;
 
-endX = startX + numLines * cellWidth;
-endY = startY + numLines * cellHeight;
+endX = startX + numCols * cellWidth;
+endY = startY + numRows * cellHeight;
 
 xvals = np.arange(startX, endX + cellWidth, cellWidth)
 yvals = np.arange(startY, endY + cellHeight, cellHeight)
@@ -35,13 +36,13 @@ def grid(DISPLAY):
     pygame.display.flip()
     
 def fillGrid(DISPLAY):
-    pygame.draw.rect(DISPLAY, BLUE, (startX, startY, cellWidth * numLines, cellHeight * numLines), 0)
+    pygame.draw.rect(DISPLAY, BLUE, (startX, startY, cellWidth * numCols, cellHeight * numRows), 0)
     pygame.display.update()
 
 def createDisk(DISPLAY, row, col, color):
-    x = int((xvals[row] + xvals[row + 1]) / 2) + 1;
-    y = int((yvals[col] + yvals[col + 1]) / 2) + 1;
-    pygame.draw.circle(DISPLAY, color, (x, y), int(cellWidth / 2) - 2, 0); 
+    x = int((xvals[col] + xvals[col + 1]) / 2) + 1;
+    y = int((yvals[row] + yvals[row + 1]) / 2) + 1;
+    pygame.draw.circle(DISPLAY, color, (x, y), int(cellWidth / 2) - 1, 0); 
 
 def main():
     pygame.init()
@@ -53,8 +54,8 @@ def main():
     grid(DISPLAY)
     fillGrid(DISPLAY)
 
-    for row in range(0, numLines):
-        for col in range(0, numLines):
+    for row in range(0, numRows):
+        for col in range(0, numCols):
             createDisk(DISPLAY, row, col, WHITE)
 
     # main loop to capture events
@@ -66,9 +67,9 @@ def main():
             if event.type == pygame.MOUSEBUTTONUP:
                 if event.button == 1:
                     x = event.pos[0];                       y = event.pos[1]
-                    rowNum = (x - startX) / cellWidth;      colNum = (y - startY) / cellHeight 
-                    if (rowNum < numLines and colNum < numLines): 
-                        print("Col Num is {} and row Num is {}".format(colNum, rowNum))
+                    colNum = (x - startX) / cellWidth;      rowNum = (y - startY) / cellHeight 
+                    if (rowNum < numRows and colNum < numCols): 
+                        print("Col Num is {} and row Num is {}".format(rowNum, colNum))
                         createDisk(DISPLAY, rowNum, colNum, GREEN)
 
         pygame.display.update()
